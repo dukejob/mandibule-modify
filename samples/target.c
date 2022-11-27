3eff7e29eb5d3a6f35f8576b671edc29216226e1
ubuntu <ubuntu@localhost.localdomain>
2022-11-27 19:57:22 +0800

    mod

---
+++ samples/target.c
@@ -0,0 +1,27 @@
+// ======================================================================== //
+// author:  ixty                                                       2018 //
+// project: mandibule                                                       //
+// licence: beerware                                                        //
+// ======================================================================== //
+
+// small example of a target program we inject code into
+
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int ac, char ** av, char ** env)
+{
+	printf("main addr %p\n", (void*)main);
+    	printf("> started.\n");
+
+    while(1)
+    {
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+    }
+
+    printf("> done.\n");
+    return 0;
+}
